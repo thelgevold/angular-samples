2577867a4e1e5dd87fec7ac299639c79fd6debcd
Torgeir Helgevold <torgeirh@hotmail.com>
2015-12-08 22:06:43 -0500

    upgraded to alpha.49

--- components/web-workers/la/la-loader.ts
+++ components/web-workers/la/la-loader.ts
@@ -1,6 +1,6 @@
 importScripts("https://code.angularjs.org/tools/system.js",
     "https://code.angularjs.org/tools/typescript.js",
-    "https://code.angularjs.org/2.0.0-alpha.48/web_worker/worker.dev.js",
+    "https://code.angularjs.org/2.0.0-alpha.49/web_worker/worker.dev.js",
     "../../../bower_components/moment/moment.js",
     "../config.js");
 
