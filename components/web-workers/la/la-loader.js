b086ff1d502fe4879765f68cb93441b399d9f69d
Torgeir Helgevold <torgeirh@hotmail.com>
2015-12-08 22:50:43 -0500

    alpha.50 upgrade

--- components/web-workers/la/la-loader.js
+++ components/web-workers/la/la-loader.js
@@ -1,3 +1,3 @@
-importScripts("https://code.angularjs.org/tools/system.js", "https://code.angularjs.org/tools/typescript.js", "https://code.angularjs.org/2.0.0-alpha.49/web_worker/worker.dev.js", "../../../bower_components/moment/moment.js", "../config.js");
+importScripts("https://code.angularjs.org/tools/system.js", "https://code.angularjs.org/tools/typescript.js", "https://code.angularjs.org/2.0.0-alpha.50/web_worker/worker.dev.js", "../../../bower_components/moment/moment.js", "../config.js");
 System.import('./la.ts');
 //# sourceMappingURL=la-loader.js.map
--- components/web-workers/new-york/new-york-loader.js
+++ components/web-workers/new-york/new-york-loader.js
@@ -1,3 +1,3 @@
-importScripts("https://code.angularjs.org/tools/system.js", "https://code.angularjs.org/tools/typescript.js", "https://code.angularjs.org/2.0.0-alpha.49/web_worker/worker.dev.js", "../../../bower_components/moment/moment.js", "../config.js");
+importScripts("https://code.angularjs.org/tools/system.js", "https://code.angularjs.org/tools/typescript.js", "https://code.angularjs.org/2.0.0-alpha.50/web_worker/worker.dev.js", "../../../bower_components/moment/moment.js", "../config.js");
 System.import('./new-york.ts');
 //# sourceMappingURL=new-york-loader.js.map
