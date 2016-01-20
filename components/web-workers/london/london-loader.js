38b7f517ad8e473b2f0ba97a0b5bc8625b8e34cc
Torgeir Helgevold <torgeirh@hotmail.com>
2016-01-19 20:01:35 -0500

    minify moment

--- components/web-workers/london/london-loader.js
+++ components/web-workers/london/london-loader.js
@@ -1,2 +1,3 @@
-importScripts("https://code.angularjs.org/tools/system.js", "https://code.angularjs.org/tools/typescript.js", "https://code.angularjs.org/2.0.0-beta.1/web_worker/worker.dev.js", "../../../bower_components/moment/moment.js", "../../../node_modules/reflect-metadata/Reflect.js", "../../../node_modules/zone.js/dist/zone.min.js", "../config.js");
+importScripts("https://code.angularjs.org/tools/system.js", "https://code.angularjs.org/tools/typescript.js", "https://code.angularjs.org/2.0.0-beta.1/web_worker/worker.dev.js", "../../../bower_components/moment/min/moment.min.js", "../../../node_modules/reflect-metadata/Reflect.js", "../../../node_modules/zone.js/dist/zone.min.js", "../config.js");
 System.import('./london.ts');
+//# sourceMappingURL=london-loader.js.map
--- components/web-workers/new-york/new-york-loader.ts
+++ components/web-workers/new-york/new-york-loader.ts
@@ -1,7 +1,7 @@
 importScripts("https://code.angularjs.org/tools/system.js",
     "https://code.angularjs.org/tools/typescript.js",
     "https://code.angularjs.org/2.0.0-beta.1/web_worker/worker.dev.js",
-    "../../../bower_components/moment/moment.js",
+    "../../../bower_components/moment/min/moment.min.js",
     "../../../node_modules/reflect-metadata/Reflect.js",
     "../../../node_modules/zone.js/dist/zone.min.js",
     "../config.js");
