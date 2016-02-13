ac049437c95ea650c8f93f0daa1ff186eb7148e1
Torgeir Helgevold <torgeirh@hotmail.com>
2016-02-13 11:38:35 -0500

    added links

--- components/log-tail/log-demo.ts
+++ components/log-tail/log-demo.ts
@@ -22,6 +22,7 @@ import {LogEntry} from './log-entry';
                         <td>{{log.severity}}</td>
                      </tr>
                 </table>
+                <h4><a href="http://www.syntaxsuccess.com/viewarticle/redux-in-angular-2.0">Read more here</a></h4>
               </div>`
 })
 
