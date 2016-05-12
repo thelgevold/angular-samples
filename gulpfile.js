86e4c5acedff79efffd0800abfb6456bc5ae72f0
Torgeir Helgevold <torgeirh@hotmail.com>
2016-05-11 23:48:17 -0400

    changed bundler

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,15 @@ var gulp = require('gulp');
 
 gulp.task('bundle', function() {
     var SystemBuilder = require('systemjs-builder');
-    var argv = require('yargs').argv;
     var builder = new SystemBuilder();
 
     builder.loadConfig('./system.config.js')
         .then(function(){
-            var outputFile = argv.prod ? 'dist/bundle.min.js' : 'dist/bundle.js';
+            var outputFile = 'dist/bundle.js';
             return builder.buildStatic('app', outputFile, {
-                minify: argv.prod,
-                mangle: argv.prod,
-                rollup: argv.prod
+                minify: true,
+                mangle: true,
+                rollup: true
             });
         })
         .then(function(){
@@ -19,17 +18,3 @@ gulp.task('bundle', function() {
         });
 });
 
-var minify = require('gulp-minify');
-
-gulp.task('compress', function() {
-    gulp.src('dist/*.js')
-        .pipe(minify({
-            ext:{
-                src:'-debug.js',
-                min:'.js'
-            },
-            exclude: ['tasks'],
-            ignoreFiles: ['.combo.js', '-min.js']
-        }))
-        .pipe(gulp.dest('dist'))
-});
