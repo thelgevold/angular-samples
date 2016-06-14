fd4cf7a6c0588d95ec1da3d7c8dc980dba65885a
Torgeir Helgevold <torgeirh@hotmail.com>
2016-06-14 18:38:45 -0400

    working in perf

--- components/web-workers/new-york/new-york-loader.js
+++ components/web-workers/new-york/new-york-loader.js
@@ -1,3 +1,3 @@
-importScripts("https://code.angularjs.org/tools/system.js", "https://code.angularjs.org/tools/typescript.js", "../../../bower_components/moment/min/moment.min.js", "../../../node_modules/reflect-metadata/Reflect.js", "../../../node_modules/zone.js/dist/zone.min.js", "../../../node_modules/zone.js/dist/long-stack-trace-zone.min.js", "../config.js");
+importScripts("https://code.angularjs.org/2.0.0-beta.17/angular2-polyfills.js", "https://code.angularjs.org/tools/system.js", "https://code.angularjs.org/tools/typescript.js", "../../../bower_components/moment/min/moment.min.js", "../config.js");
 System.import('./new-york.ts');
 //# sourceMappingURL=new-york-loader.js.map
