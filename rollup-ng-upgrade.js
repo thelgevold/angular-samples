8d8cbd556035d3b377e96e40af0244a449cd5559
Torgeir Helgevold <torgeirh@hotmail.com>
2016-10-23 16:51:22 -0400

    refactor ng-upgrade code

--- rollup-ng-upgrade.js
+++ rollup-ng-upgrade.js
@@ -2,31 +2,19 @@ import rollup      from 'rollup'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs    from 'rollup-plugin-commonjs';
 import uglify      from 'rollup-plugin-uglify';
-import angular     from 'rollup-plugin-angular';
-import typescript  from 'rollup-plugin-typescript';
 import closure     from 'google-closure-compiler-js';
-
-function closureCompilerPlugin(){
-  return {
-    transformBundle(bundle){
-      var transformed = closure.compile({compilationLevel: 'SIMPLE', jsCode: [{src: bundle}]});
-      return transformed.compiledCode;
-    }
-  }
-}
 
 export default {
   entry: 'upgrade.js',
-  dest: 'upgrade-build.js',
-  sourceMap: false,
+  dest: './dist/upgrade-build.js',
+  sourceMap: true,
+  sourceMapFile: './dist/upgrade-build.js.map',
   format: 'iife',
   plugins: [
-      angular(),
-      typescript(),
       nodeResolve({jsnext: true, module: true}),
       commonjs({
         include: 'node_modules/rxjs/**',
       }),
-      closureCompilerPlugin()
+      uglify()
   ]
 }
