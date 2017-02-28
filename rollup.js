7c8188d88fee89662da372e6ac888de897ae1cd7
Torgeir Helgevold <torgeirh@hotmail.com>
2017-02-27 23:05:55 -0500

    test bundle

--- rollup.js
+++ rollup.js
@@ -1,6 +1,30 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs    from 'rollup-plugin-commonjs';
 import uglify      from 'rollup-plugin-uglify';
+import { createFilter } from 'rollup-pluginutils';
+
+class RollupRemoveDecorators {
+    
+    transform(code, id) {
+      if ( id.indexOf('node_modules') !== -1) return null;
+
+      let pattern = /(.*).ctorParameters/;
+      var matches = pattern.exec(code);
+      if(matches){
+        let key = matches[1] + '.decorators';
+        console.log(key);
+        console.log(matches[1] + '.ctorParameters');
+        let indexCtorParameters = code.indexOf(matches[1] + '.ctorParameters');
+        let indexDecorator = code.indexOf(key);
+       
+        let trimmed = code.substr(0, indexDecorator) + code.substr(indexCtorParameters);
+        return {code: trimmed};
+      }
+
+      return null;
+    }
+}
+const removeDecorators = (config) => new RollupRemoveDecorators(config);
 
 export default {
     entry: 'main.js',
@@ -14,7 +38,9 @@ export default {
       nodeResolve({
           jsnext: true, module: true
       }),
+      new RollupRemoveDecorators(),
       uglify()
+      
     ]
 
 }
