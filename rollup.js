5d40833d31120a026d8989271e0f5713d9c53d3e
Torgeir Helgevold <torgeirh@hotmail.com>
2017-02-27 23:53:31 -0500

    testing

--- rollup.js
+++ rollup.js
@@ -1,24 +1,22 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs    from 'rollup-plugin-commonjs';
 import uglify      from 'rollup-plugin-uglify';
-import { createFilter } from 'rollup-pluginutils';
 
 class RollupRemoveDecorators {
     
     transform(code, id) {
-      if ( id.indexOf('node_modules') !== -1) return null;
-
       let pattern = /(.*).ctorParameters/;
       var matches = pattern.exec(code);
       if(matches){
         let key = matches[1] + '.decorators';
-        console.log(key);
-        console.log(matches[1] + '.ctorParameters');
         let indexCtorParameters = code.indexOf(matches[1] + '.ctorParameters');
         let indexDecorator = code.indexOf(key);
-       
-        let trimmed = code.substr(0, indexDecorator) + code.substr(indexCtorParameters);
-        return {code: trimmed};
+        
+        if(indexDecorator == -1) return;
+
+        let transformed = code.substr(0, indexDecorator) + code.substr(indexCtorParameters);
+        return {code: transformed}; 
+        
       }
 
       return null;
@@ -39,7 +37,7 @@ export default {
           jsnext: true, module: true
       }),
       new RollupRemoveDecorators(),
-      uglify()
+      //uglify()
       
     ]
 
