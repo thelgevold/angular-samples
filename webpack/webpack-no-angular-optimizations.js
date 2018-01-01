78be314bcf8df2bd4e4fed3151eb71518022ac44
Torgeir Helgevold <torgeirh@hotmail.com>
2018-01-01 09:55:10 -0500

    demo without webpack angular optimizations

---
+++ webpack/webpack-no-angular-optimizations.js
@@ -0,0 +1,35 @@
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './built-es5/app/main.js',
+  output: {
+    filename: 'dist/bundle-webpack-3-no-angular-optimizations.js'
+  },
+
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new UglifyJsPlugin({
+      beautify: false, 
+      output: {
+        comments: false
+      }, 
+      mangle: {
+        screw_ie8: true
+      }, 
+      compress: {
+        screw_ie8: true,
+        warnings: false,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+        negate_iife: false
+      },
+    })
+  ]
+}
