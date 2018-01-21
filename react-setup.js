c5658ebbbd17d5e0864f5878ac7c96e5cb424868
Tor <torgeirh@hotmail.com>
2018-01-21 13:04:31 -0500

    remove unused code

--- react-setup.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var __React = window['React'];
--- react-setup.ts
+++
@@ -1,4 +0,0 @@
-var __React = window['React'];
-//var Dispatcher = require('./node_modules/flux/dist/Flux.min').Dispatcher;
-//var EventEmitter = require('./node_modules/events/events').EventEmitter;
-import Dispatcher from 'flux/dist/Flux.min';
