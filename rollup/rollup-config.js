5efcdd6d384cd0a9155346efaf589b8ff76a4c2a
Torgeir Helgevold <tor@nrwl.io>
2018-05-19 11:34:16 -0400

    clean up

--- rollup/rollup-config.js
+++ rollup/rollup-config.js
@@ -1,5 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import uglify      from 'rollup-plugin-uglify';
+import commonjs    from 'rollup-plugin-commonjs';
 
 export default {
     input: 'built/src/apps/main-demo-app/main.js',
