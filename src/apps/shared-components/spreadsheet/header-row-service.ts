c35ee68eced69a139d7587250e23eda969f81b65
Torgeir Helgevold <tor@nrwl.io>
2018-06-19 22:09:24 -0400

    formatting

--- src/apps/shared-components/spreadsheet/header-row-service.ts
+++ src/apps/shared-components/spreadsheet/header-row-service.ts
@@ -1,31 +1,28 @@
-export class HeaderRowService{
+export class HeaderRowService {
+  static createHeader(length: Number) {
+    const alpha = 'abcdefghijklmnopqrstuvwxyz';
 
-    static createHeader(length:Number){
-        const alpha = 'abcdefghijklmnopqrstuvwxyz';
+    let headerLength = 1;
 
-        let headerLength = 1;
+    let header = [];
 
-        let header = [];
+    let prefix = '';
 
-        let prefix = '';
+    let pass = 0;
 
-        let pass = 0;
+    let charCounter = 0;
 
-        let charCounter = 0;
+    for (let i = 0; i < length; i++) {
+      if (charCounter === alpha.length) {
+        prefix += alpha.substr(pass, 1);
+        charCounter = 0;
+      }
 
-        for(let i = 0; i < length; i++){
+      header.push(prefix + alpha.substr(charCounter, headerLength));
 
-            if(charCounter === alpha.length){
-                prefix += alpha.substr(pass,1);
-                charCounter = 0;
-            }
-
-            header.push(prefix + alpha.substr(charCounter,headerLength));
-
-            charCounter++;
-        }
-
-        return header;
+      charCounter++;
     }
 
+    return header;
+  }
 }
