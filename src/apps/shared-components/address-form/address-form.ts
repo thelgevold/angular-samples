fcb02623e35b7f2070c9ffa314997d057a355d39
Torgeir Helgevold <tor@nrwl.io>
2018-11-04 10:08:56 -0500

    clean up

--- src/apps/shared-components/address-form/address-form.ts
+++ src/apps/shared-components/address-form/address-form.ts
@@ -1,38 +1,35 @@
-function zipValidator(zip) {
-
-    var valid = /^\d{5}$/.test(zip.value);
-
-    if(valid){
-        return null;
-    }
-
-    return {"invalidZip":true};
-}
-
 import {Component} from '@angular/core';
 import {FormGroup, Validators, FormControl} from '@angular/forms';
 
+function zipValidator(zip) {
+  var valid = /^\d{5}$/.test(zip.value);
+
+  if (valid) {
+    return null;
+  }
+
+  return {invalidZip: true};
+}
+
 @Component({
-    selector: 'address-form',
-    templateUrl: './address-form.html'
+  selector: 'address-form',
+  templateUrl: './address-form.html',
 })
-
 export class AddressForm {
+  form;
+  payLoad = null;
 
-    form;
-    payLoad = null;
+  constructor() {
+    let group: any = {};
+    group.firstName = new FormControl('', Validators.required);
+    group.streetAddress = new FormControl('', Validators.required);
+    group.zip = new FormControl('', zipValidator);
+    group.type = new FormControl('home');
 
-    constructor() {
-        let group:any = {};
-        group.firstName =  new FormControl('', Validators.required);
-        group.streetAddress = new FormControl('', Validators.required);
-        group.zip = new FormControl('', zipValidator);
-        group.type = new FormControl('home');
+    this.form = new FormGroup(group);
+  }
 
-        this.form = new FormGroup(group);
-    }
-
-    onSubmit() {
-        this.payLoad = JSON.stringify(this.form.value);
-    }
+  onSubmit() {
+    this.payLoad = JSON.stringify(this.form.value);
+  }
 }
